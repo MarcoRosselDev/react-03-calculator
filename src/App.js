7996825b8e1195e7414d82aa94617f4b08bb2df1
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-27 07:18:14 -0400

    fist commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//find . -name 'node_modules' -type d -prune -print -exec rm -rf '{}' \;
+// comando para terminal linux delete all _modules no usados
+// en caso de trabajar en un proyecto denuevo usar npm install y listo
