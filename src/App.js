fddd4d9310a4a1c3e9bc60b39c25ae6c816ee732
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-27 21:29:49 -0400

    imgLogo freeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import imgLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={imgLogo} 
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
